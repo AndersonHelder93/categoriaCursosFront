a3503b9da618cc2c7ba35a5b0610d259af22a5ff
AndersonHelder93 <andersonhelder18@gmail.com>
2022-07-12 22:17:33 -0300

    Front end da aplicacao de listar e categorizar cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Configuração de rotas das páginas e componentes, verificando o disparo no navegador antes de
+//ralizar o redirecionamento para o componente courses
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',//caminho
+    //caminho do import é onde está localizado o arquivo do módulo filho
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
